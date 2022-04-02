34cc0067438c4c76089092d74ad332a7ee0a5643
Athindu <athinduama@gmail.com>
2022-04-02 23:44:37 +0530

    Fix weather API

---
+++ lib/utils/forecast.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:weather_data/utils/location.dart';
+
+const apiKey = 'aeb4d3b3f6307933ef76de4e8c597022';
+
+class WeatherData {
+
+  LocationHelper locationData;
+  late double currentTemperature;
+  late String currentCondition;
+  late String city;
+  late double feels_like;
+  late double temp_min;
+  late double temp_max;
+  //WeatherData(required this.locationData);
+
+  WeatherData({required this.locationData});
+
+  Future<void> getCurrentTemperature() async {
+    Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=${locationData.latitude}&lon=${locationData.longitude}&appid=${apiKey}&units=metric'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var currentWeather = jsonDecode(data);
+
+      try {
+        currentTemperature = currentWeather['main']['temp'];
+        currentCondition = currentWeather['weather'][0]['description'];
+        city = currentWeather['name'];
+        feels_like = currentWeather['main']['feels_like'];
+        temp_min = currentWeather['main']['temp_min'];
+        temp_max = currentWeather['main']['temp_max'];
+
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      print('Could not fetch temperature!');
+    }
+  }
+
+  void getNormalWeather() {
+    currentTemperature = 25.73;
+    currentCondition = "scattered clouds";
+    city = "Matara";
+    feels_like = 26.5;
+    temp_min = 25.68;
+  }
+
+}
